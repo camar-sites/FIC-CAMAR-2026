23c5f68ae27cb576f7b4a43129faeb26a222d6fb
Sandro Dias Martins <sandrodiasmartins@gmail.com>
2026-06-26 14:25:22 -0300

    Add files via upload

---
+++ src/components/AvaliacaoForm.tsx
@@ -0,0 +1,12 @@
+// src/components/AvaliacaoForm.tsx
+const AvaliacaoForm = () => (
+  <iframe
+    src="https://docs.google.com/forms/d/e/1FAIpQLSekFP0-8ZBZ6vjG12am9cmWIPAhYWaG3YQyDdz92VVXT4eS6Q/viewform?embedded=true"
+    width="100%"
+    height="800"
+    frameBorder="0"
+    title="FIC CAMAR 2026 - Avaliação Geral"
+  />
+);
+
+export default AvaliacaoForm;
